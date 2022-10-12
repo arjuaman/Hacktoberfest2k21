b009abf2b8f1ef687b5aac902cfd1d0590e72a02
Ayush Aman <72867624+ayushaman15@users.noreply.github.com>
2022-10-12 23:50:22 +0530

    dist btn two points

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 int main()
 {
-    float a,b,c,greatest;
-    printf("Enter the three numbers you want to compare:");
-    scanf("%f %f %f",&a,&b,&c);
-    greatest=(a>b)?((a>c)?a:c):((b>c)?b:c) ;
-    printf("The greatest number is %0.3f",greatest);
-
+    float x1,x2,y1,y2;
+    printf("Enter the coordinates of 1st point:");
+    scanf("%f %f",&x1,&y1);
+    printf("Enter the coordinates of 2nd point:");
+    scanf("%f %f",&x2,&y2);
+    float dist;
+    dist=sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+    printf("The calculated distance is: %f",dist);
     return 0;
 }
