e732d1668d908bef82f7c0bfef23a3ccbfbf3d28
Ayush Aman <72867624+ayushaman15@users.noreply.github.com>
2022-10-12 23:48:11 +0530

    Ternary operators

--- main.c
+++ main.c
@@ -3,32 +3,11 @@
 
 int main()
 {
-    int i,pos,num,n,arr[10];
-    printf("Enter the length of array: ");
-    scanf("%d",&n);
-    for(int k=0;k<n;k++){
-        scanf("%d",&arr[k]);
-    }
-    printf("\n\n");
-    for(int k=0;k<n;k++){
-        printf(" %d ",arr[k]);
-    }
-    printf("\n Enter the position at where to enter new element: ");
-    scanf("%d",&pos);
-    printf("\n Enter the new element: ");
-    scanf("%d",&num);
-    //pos=pos-1;
-    for(i=n+1;i>=pos;i--){
-        arr[i]=arr[i-1];
-        if(i==pos){
-            arr[i]=num;
-            break;
-            //arr[pos]=num;
-        }
-    }
-    printf("\n\n");
-    for(int j=0;j<n+1;j++){
-            printf(" %d ",arr[j]);
-    }
+    float a,b,c,greatest;
+    printf("Enter the three numbers you want to compare:");
+    scanf("%f %f %f",&a,&b,&c);
+    greatest=(a>b)?((a>c)?a:c):((b>c)?b:c) ;
+    printf("The greatest number is %0.3f",greatest);
+
     return 0;
 }
